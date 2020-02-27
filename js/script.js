e2111ddb6f77908694f07e6a65917b0c0df6d128
Margaret Wairimu Kimani-1040 <kimanimargaret46@gmail.com>
2020-02-27 12:31:19 +0300

    added information and images in the about page

--- js/script.js
+++ js/script.js
@@ -5,20 +5,7 @@ $(document).ready(function () {
 });
 
 
-$(document).ready(function () {
-    $(".onCrack1").click(function () {
-        $("#click1").toggle();
-        $("#magic1").toggle();
-    });
-    $(".onCrack2").click(function () {
-        $("#magic2").toggle();
-        $("#click2").toggle();
-    });
-    $(".onCrack3").click(function () {
-        $("#magic3").toggle();
-        $("#click3").toggle();
-    });
-});
+
 
 
 $(document).ready(function () {
