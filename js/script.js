006b491a2a61623f184290b26bde1f9b2425229e
Margaret Wairimu Kimani-1040 <kimanimargaret46@gmail.com>
2020-02-28 13:00:13 +0300

    added a gif and menu page content

--- js/script.js
+++ js/script.js
@@ -5,6 +5,28 @@ $(document).ready(function () {
 });
 
 
+$(document).ready(function () {
+    $("#trigger").click(function () {
+        $("#menu").toggle();
+    });
+});
+
+
+$(document).ready(function () {
+    $(".onCrack1").click(function () {
+        $("#click1").toggle();
+        $("#magic1").toggle();
+    });
+    $(".onCrack2").click(function () {
+        $("#magic2").toggle();
+        $("#click2").toggle();
+    });
+    $(".onCrack3").click(function () {
+        $("#magic3").toggle();
+        $("#click3").toggle();
+    });
+});
+
 
 
 
